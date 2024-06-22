71fe9a1f523c141e1217c3fdb4183f67358043cb
Prashant-Bista <bistaprashant1@gmail.com>
2024-06-22 15:22:48 +0545

    updated contact details, and about us

--- lib/components.dart
+++ lib/components.dart
@@ -45,3 +45,34 @@ class _TabsWebState extends State<TabsWeb> {
         ));
   }
 }
+
+class SansBold extends StatelessWidget {
+  final text;
+  final size;
+
+  const SansBold(this.text,this.size,{super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: GoogleFonts.openSans(
+          fontSize: size, fontWeight: FontWeight.bold),
+    );
+  }
+}
+class Sans extends StatelessWidget {
+  final text;
+  final size;
+
+  const Sans(this.text,this.size,{super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: GoogleFonts.openSans(
+          fontSize: size
+    ));
+  }
+}
--- lib/web/landing_page_web.dart
+++ lib/web/landing_page_web.dart
@@ -12,7 +12,7 @@ class LandingPageWeb extends StatefulWidget {
 class _LandingaPageMobileState extends State<LandingPageWeb> {
   @override
   Widget build(BuildContext context) {
-    double heightdevice = MediaQuery.of(context).size.height;
+    var heightdevice = MediaQuery.of(context).size.height;
     return Scaffold(
       drawer: Drawer(//To create a drawer menu
 
@@ -46,27 +46,54 @@ class _LandingaPageMobileState extends State<LandingPageWeb> {
         Container(
           height: heightdevice - 56,
           child: Row(
-            mainAxisAlignment:MainAxisAlignment.spaceAround ,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               Column(
-                mainAxisAlignment:MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
+                    //
                     decoration: BoxDecoration(
-                      color: Colors.tealAccent,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(25),
-                        topRight:Radius.circular(25),
-                        bottomRight:Radius.circular(25),
-                      )
-                    ),padding: EdgeInsets.symmetric(vertical: 10,horizontal: 20),
-                      child: Text(
-                    "Hello, I'm",
-                    style: GoogleFonts.openSans(
-                        fontSize: 15, fontWeight: FontWeight.bold),
-                  ))
+                        color: Colors.tealAccent,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(25),
+                          topRight: Radius.circular(25),
+                          bottomRight: Radius.circular(25),
+                        )),
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                    child: SansBold("Hello, I'm",20.0),
+
+                  ),
+                  SizedBox(height: 15),
+                  SansBold("Prashant Bista", 55.0),
+                  Sans("Learning Flutter",22.0),
+                  SizedBox(height: 15.0,),
+                  Row(
+                    children: [
+                      Icon(Icons.email),
+                      SizedBox(width: 20,),
+                      Sans("bistaprashant@gmail.com", 15.0)
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.phone),
+                      SizedBox(width: 20,),
+                      Sans("9812345678", 15.0)
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.location_pin),
+                      SizedBox(width: 20,),
+                      Sans("221B, Baker Street, UK", 15.0)
+                    ],
+                  )
                 ],
               ),
+              //multiple circleAvatars of slightly different sizes to
+              //create a border of different colors
               CircleAvatar(
                 backgroundColor: Colors.black,
                 radius: 139,
@@ -83,6 +110,97 @@ class _LandingaPageMobileState extends State<LandingPageWeb> {
             ],
           ),
         ),
+        //stating the begining of the second page
+        Container(
+          height: heightdevice/1.5,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset("assets/web.jpg"),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SansBold("About Me", 40.0),
+                  SizedBox(height: 15.0,),
+                  SizedBox(width: 15.0,),
+                  Sans("Hello, I'm Prashant Bista. I speacialize in flutter UI Development.", 15.0),
+                  SizedBox(height: 15,),
+                  Sans("I don't plan to stay a frontend developer only. Soon, there will be more from me.", 15.0),
+                  SizedBox(height: 10.0,),
+                  Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.tealAccent,
+                          style: BorderStyle.solid,
+                          width: 2.0,
+              
+                        ),
+              borderRadius: BorderRadius.circular(7),
+                        ),
+                        child: Sans("Flutter",15.0),
+                      ),
+                      SizedBox(width: 7,),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.tealAccent,
+                            style: BorderStyle.solid,
+                            width: 2.0,
+              
+                          ),
+                          borderRadius: BorderRadius.circular(7),
+                        ),
+                        child: Sans("Firebase",15.0),
+                      ),
+                      SizedBox(width: 7,),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.tealAccent,
+                            style: BorderStyle.solid,
+                            width: 2.0,
+              
+                          ),
+                          borderRadius: BorderRadius.circular(7),
+                        ),
+                        child: Sans("Android",15.0),
+                      ),
+                      SizedBox(width: 7,),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.tealAccent,
+                            style: BorderStyle.solid,
+                            width: 2.0,
+              
+                          ),
+                          borderRadius: BorderRadius.circular(7),
+                        ),
+                        child: Sans("IOS",15.0),
+                      ),
+                      SizedBox(width: 7,),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.tealAccent,
+                            style: BorderStyle.solid,
+                            width: 2.0,
+              
+                          ),
+                          borderRadius: BorderRadius.circular(7),
+                        ),
+                        child: Sans("Linux",15.0),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        )
       ]),
     );
   }
