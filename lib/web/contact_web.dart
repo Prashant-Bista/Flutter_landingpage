5a9c58265d8b0f7b69e26078c4105290e6228b1f
Prashant-Bista <bistaprashant1@gmail.com>
2024-07-13 20:17:57 +0545

    created Contact page for Web

--- lib/web/contact_web.dart
+++ lib/web/contact_web.dart
@@ -15,8 +15,7 @@ class _ContactWebState extends State<ContactWeb> {
   Widget build(BuildContext context) {
     var widthDevice = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(),
-      endDrawer: Drawer(
+      drawer: Drawer(
         backgroundColor: Colors.white,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -75,6 +74,129 @@ class _ContactWebState extends State<ContactWeb> {
         ),
       ),
       backgroundColor: Colors.white,
+      body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                title: Row(
+                  children: [
+                    Spacer(
+                      flex: 3,
+                    ),
+                    TabsWeb(
+                      title: "Home",
+                      route: '/',
+                    ),
+                    Spacer(),
+                    TabsWeb(
+                      title: "Works",
+                      route: '/works',
+                    ),
+                    Spacer(),
+                    TabsWeb(
+                      title: "Blog",
+                      route: '/blog',
+                    ),
+                    Spacer(),
+                    TabsWeb(
+                      title: "About",
+                      route: '/about',
+                    ),
+                    Spacer(),
+                    TabsWeb(
+                      title: "Contact",
+                      route: '/',
+                    ),
+                    Spacer(),
+                  ],
+                ),
+                expandedHeight: 600.0,
+                backgroundColor: Colors.white,
+                iconTheme: IconThemeData(
+                  size: 25.0,
+                  color: Colors.black,
+                ),
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Image.asset(
+                    "assets/contact_image.jpg",
+                    fit: BoxFit.cover,
+                    filterQuality: FilterQuality.high,
+                  ),
+                ),
+              ),
+            ];
+          },
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 30.0,
+                ),
+                SansBold("Contact me", 40.0),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        TextForm(
+                            hintText: "Please Enter your Firstname",
+                            text: "First Name",
+                            Containerwidth: 350.0),
+                        SizedBox(
+                          height: 25.0,
+                        ),
+                        TextForm(
+                            hintText: "Please Enter your Email address",
+                            text: "Email",
+                            Containerwidth: 350.0)
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        TextForm(
+                            hintText: "Please Enter your Last Name",
+                            text: "Last Name",
+                            Containerwidth: 350.0),
+                        SizedBox(
+                          height: 25.0,
+                        ),
+                        TextForm(
+                            hintText: "Please Enter your Phone Number",
+                            text: "Phone NUmber",
+                            Containerwidth: 350.0)
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                TextForm(
+                  hintText: ("Please Enter your Message"),
+                  text: "Message",
+                  Containerwidth: widthDevice / 1.5,
+                  maxlines: 10,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                MaterialButton(
+                  child: Sans("Submit", 33.0),
+                  onPressed: () {},
+                  elevation: 20.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  color: Colors.lightBlueAccent,
+                  height: 60.0,
+                  minWidth: 200.0,
+                )
+              ],
+            ),
+          )),
     );
   }
 }
